39e6fc3cb126347f63dc04ebb88721168ba5ed4b
Andrei Trifu <Andrei.Trifu84@gmail.com>
2023-10-25 16:34:10 +0100

    Book webpage exercise in React

---
+++ src/components/BookListing/BookListing.jsx
@@ -0,0 +1,16 @@
+import "./BookListing.css";
+function BookListing({ id, title, author, image, genre, setClickedBook }) {
+  function bookClick() {
+    setClickedBook(id);
+  }
+  return (
+    <div className="booklisting" onClick={bookClick}>
+      <h3>{title}</h3>
+      <h4>{author}</h4>
+      <img src={image} alt={title} />
+      <p>{genre}</p>
+    </div>
+  );
+}
+
+export default BookListing;
